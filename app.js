e75f0b98656b6bdb7c2673b083c71c5ef862a4b0
Chris Costanza <christopherjohncostanza@gmail.com>
2022-03-02 11:03:26 -0500

    updated links

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const resultsList = document.querySelector("#results")
-const searchForm = document.querySelector("#searchForm")
+const resultsList = document.querySelector("#results");
+const searchForm = document.querySelector("#searchForm");
 
 const showSearch = async (searchTerm) => {
     const config = { params: { q: searchTerm } }
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
     for (show of res.data) {
-        const newLI = document.createElement('LI')
-        const newIMG = document.createElement('IMG')
+        const newLI = document.createElement('LI');
+        const newIMG = document.createElement('IMG');
+        const newA = document.createElement('A');
+        newA.href = show.show.url
+        console.dir(newA)
         if (show.show.image) {
             newIMG.src = show.show.image.medium;
             newIMG.classList = "movieThumb"
         }
-
-        newLI.append(show.show.name, newIMG)
+        newA.append(show.show.name)
+        newLI.append(newA, newIMG)
         resultsList.append(newLI)
     }
 }
